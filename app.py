0b5c8664800016ac193cd56f04db1b28ef4395c9
Becky Rushton <beckyrushton30@gmail.com>
2023-05-22 18:33:27 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+# name = input("What is your name? ")
+# birth_year = input("Enter your birth year: ")
+# age = 2023 - int(birth_year)
+# print("Hello " + name)
+# print("You are ")
+# print(age)
+# print("years old.")
+
+numOne = input("Enter first number: ")
+numTwo = input("Enter second number: ")
+Sum = float(numOne) + float(numTwo)
+print("The sum of these two numbers is " + str(Sum))
